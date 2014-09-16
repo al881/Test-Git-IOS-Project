713c739232f53245a6a8b26737a966f59116dd1c
Al Butler <abutler@emida.net>
2014-09-16 19:31:04 -0400

    added a test class

---
+++ Git Test/ABTest.h
@@ -0,0 +1,13 @@
+//
+//  ABTest.h
+//  Git Test
+//
+//  Created by Al Butler on 9/16/14.
+//  Copyright (c) 2014 Al Butler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABTest : NSObject
+
+@end
---
+++ Git Test/ABTest.m
@@ -0,0 +1,13 @@
+//
+//  ABTest.m
+//  Git Test
+//
+//  Created by Al Butler on 9/16/14.
+//  Copyright (c) 2014 Al Butler. All rights reserved.
+//
+
+#import "ABTest.h"
+
+@implementation ABTest
+
+@end
